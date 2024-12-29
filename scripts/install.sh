929583917f7b5b6cd29e294daa05b21023b2809b
ggindinson <97807772+ggindinson@users.noreply.github.com>
2024-12-29 11:48:54 +0300

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -31,79 +31,21 @@ echo "Installing A1111 Web UI"
 wget https://raw.githubusercontent.com/ashleykleynhans/runpod-worker-a1111/main/install-automatic.py
 python3 -m install-automatic --skip-torch-cuda-test
 
-echo "Cloning ControlNet extension repo"
-cd /workspace/stable-diffusion-webui
-git clone --depth=1 https://github.com/Mikubill/sd-webui-controlnet.git extensions/sd-webui-controlnet
-
-echo "Cloning the ReActor extension repo"
-git clone https://github.com/Gourieff/sd-webui-reactor.git extensions/sd-webui-reactor
-git checkout v0.6.1
-
-echo "Cloning the After Detailer extension repo"
-git clone --depth=1 https://github.com/Bing-su/adetailer.git extensions/adetailer
-
-echo "Installing dependencies for ControlNet"
-cd /workspace/stable-diffusion-webui/extensions/sd-webui-controlnet
-pip3 install -r requirements.txt
-
-echo "Installing dependencies for ReActor"
-cd /workspace/stable-diffusion-webui/extensions/sd-webui-reactor
-pip3 install -r requirements.txt
-pip3 install onnxruntime-gpu
-
-echo "Installing dependencies for After Detailer"
-cd /workspace/stable-diffusion-webui/extensions/adetailer
-python3 -m install
-
-echo "Installing the model for ReActor"
-mkdir -p /workspace/stable-diffusion-webui/models/insightface
-cd /workspace/stable-diffusion-webui/models/insightface
-aria2c -o inswapper_128.onnx https://github.com/facefusion/facefusion-assets/releases/download/models/inswapper_128.onnx
-
-echo "Configuring ReActor to use the GPU instead of CPU"
-echo "CUDA" > /workspace/stable-diffusion-webui/extensions/sd-webui-reactor/last_device.txt
-
 echo "Installing RunPod Serverless dependencies"
 cd /workspace/stable-diffusion-webui
 pip3 install huggingface_hub runpod
 
-echo "Downloading Deliberate v2 model"
+echo "Downloading AAM XL Model"
 cd /workspace/stable-diffusion-webui/models/Stable-diffusion
-aria2c -o deliberate_v2.safetensors https://huggingface.co/ashleykleynhans/a1111-models/resolve/main/Stable-diffusion/deliberate_v2.safetensors
+aria2c -o aam.safetensors https://civitai.com/api/download/models/303526?type=Model&format=SafeTensor&size=full&fp=fp16
 
-echo "Downloading SDXL base model"
-aria2c -o sd_xl_base_1.0.safetensors https://huggingface.co/stabilityai/stable-diffusion-xl-base-1.0/resolve/main/sd_xl_base_1.0.safetensors
+echo "Downloading Juggernaut XL Model"
+cd /workspace/stable-diffusion-webui/models/Stable-diffusion
+aria2c -o juggernaut.safetensors https://civitai.com/api/download/models/782002?type=Model&format=SafeTensor&size=full&fp=fp16
 
-echo "Downloading SDXL Refiner"
-aria2c -o sd_xl_refiner_1.0.safetensors https://huggingface.co/stabilityai/stable-diffusion-xl-refiner-1.0/resolve/main/sd_xl_refiner_1.0.safetensors
-
-echo "Downloading turboDiffusionXL v112 model"
-aria2c -o turboDiffusionXL_v112.safetensors https://huggingface.co/ashleykleynhans/a1111-models/resolve/main/Stable-diffusion/turboDiffusionXL_v112.safetensors
-
-echo "Downloading SD 1.5 VAE"
-cd /workspace/stable-diffusion-webui/models/VAE
-aria2c -o vae-ft-mse-840000-ema-pruned.safetensors https://huggingface.co/stabilityai/sd-vae-ft-mse-original/resolve/main/vae-ft-mse-840000-ema-pruned.safetensors
-
-echo "Downloading SDXL VAE"
-aria2c -o sdxl_vae.safetensors https://huggingface.co/madebyollin/sdxl-vae-fp16-fix/resolve/main/sdxl_vae.safetensors
-
-echo "Downloading SD 1.5 ControlNet models"
-mkdir -p /workspace/stable-diffusion-webui/models/ControlNet
-cd /workspace/stable-diffusion-webui/models/ControlNet
-aria2c -o control_v11p_sd15_openpose.pth https://huggingface.co/lllyasviel/ControlNet-v1-1/resolve/main/control_v11p_sd15_openpose.pth
-aria2c -o control_v11p_sd15_canny.pth https://huggingface.co/lllyasviel/ControlNet-v1-1/resolve/main/control_v11p_sd15_canny.pth
-aria2c -o control_v11f1p_sd15_depth.pth https://huggingface.co/lllyasviel/ControlNet-v1-1/resolve/main/control_v11f1p_sd15_depth.pth
-aria2c -o control_v11p_sd15_inpaint.pth https://huggingface.co/lllyasviel/ControlNet-v1-1/resolve/main/control_v11p_sd15_inpaint.pth
-aria2c -o control_v11p_sd15_lineart.pth https://huggingface.co/lllyasviel/ControlNet-v1-1/resolve/main/control_v11p_sd15_lineart.pth
-aria2c -o control_v1p_sd15_brightness.safetensors https://huggingface.co/ioclab/ioc-controlnet/resolve/main/models/control_v1p_sd15_brightness.safetensors
-aria2c -o control_v11f1e_sd15_tile.pth https://huggingface.co/lllyasviel/ControlNet-v1-1/resolve/main/control_v11f1e_sd15_tile.pth
-
-echo "Downloading SDXL ControlNet models"
-aria2c -o diffusers_xl_canny_full.safetensors https://huggingface.co/lllyasviel/sd_control_collection/resolve/main/diffusers_xl_canny_full.safetensors
-
-echo "Downloading InstantID ControlNet models"
-aria2c -o ip-adapter_instant_id_sdxl.bin "https://huggingface.co/InstantX/InstantID/resolve/main/ip-adapter.bin?download=true"
-aria2c -o control_instant_id_sdxl.safetensors "https://huggingface.co/InstantX/InstantID/resolve/main/ControlNetModel/diffusion_pytorch_model.safetensors?download=true"
+echo "Downloading DreamShaper XL Model"
+cd /workspace/stable-diffusion-webui/models/Stable-diffusion
+aria2c -o dreamshaper.safetensors https://civitai.com/api/download/models/351306?type=Model&format=SafeTensor&size=full&fp=fp16
 
 echo "Downloading Upscalers"
 mkdir -p /workspace/stable-diffusion-webui/models/ESRGAN
